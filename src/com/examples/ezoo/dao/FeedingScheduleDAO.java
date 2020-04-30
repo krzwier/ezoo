354e8bb04032f83c71070ac815e760b0016d278d
krzwier <krzwier@gmail.com>
2020-04-30 12:17:35 -0500

    Add FeedingSchedule model and FeedingSchedule DAO.

---
+++ src/com/examples/ezoo/dao/FeedingScheduleDAO.java
@@ -0,0 +1,50 @@
+package com.examples.ezoo.dao;
+
+import java.util.List;
+
+import com.examples.ezoo.model.Animal;
+import com.examples.ezoo.model.FeedingSchedule;
+
+/**
+ * Main interface used to execute CRUD methods on FeedingSchedule class
+ * @author Karen Zwier
+ *
+ */
+public interface FeedingScheduleDAO {
+
+	
+	/**
+	 * Used to persist the feeding schedule to the datastore
+	 * @param feedingScheduleToSave
+	 */
+	void saveFeedingSchedule(FeedingSchedule feedingScheduleToSave) throws Exception;
+	
+	/**
+	 * Used to delete a feeding schedule from the datastore
+	 * @param feedingScheduleToDelete
+	 */
+	void deleteFeedingSchedule(FeedingSchedule feedingScheduleToDelete) throws Exception;
+	
+	/**
+	 * Used to retrieve a list of all FeedingSchedules 
+	 * @return
+	 */
+	List<FeedingSchedule> getAllFeedingSchedules();
+	
+	/**
+	 * Used to retrieve a single feeding schedule from the datastore
+	 * for a given animal
+	 */
+	FeedingSchedule getFeedingSchedule(Animal animal);
+	
+	/**
+	 * Used to assign a feeding schedule to a particular animal
+	 */
+	void assignFeedingSchedule(Animal animal, FeedingSchedule feedingSchedule) throws Exception;
+	
+	/**
+	 * Used to unassign a feeding schedule from a particular animal
+	 */
+	void unassignFeedingSchedule(Animal animal) throws Exception;
+	
+}
